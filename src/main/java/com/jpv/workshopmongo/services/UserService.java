3d2577dc19183b8bfb40d85c1ad8880e172b6015
Jeff Valim <jpvalim@hotmail.com>
2021-10-14 22:53:15 -0300

    Get user by id, notFound exception

--- src/main/java/com/jpv/workshopmongo/services/UserService.java
+++ src/main/java/com/jpv/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.jpv.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jpv.workshopmongo.domain.User;
 import com.jpv.workshopmongo.repositories.UserRepository;
+import com.jpv.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,9 @@ public class UserService {
 		return repository.findAll();
 	}
 	
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return	obj.orElseThrow(()-> new ObjectNotFoundException("Could not find the object"));
+	}
 }
