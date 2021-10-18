8e2b8a98d75089d1a9521fffaf2e0ea6679c66be
Jeff Valim <jpvalim@hotmail.com>
2021-10-18 17:23:38 -0300

    Post Array on Users

--- src/main/java/com/jpv/workshopmongo/config/Instantiation.java
+++ src/main/java/com/jpv/workshopmongo/config/Instantiation.java
@@ -41,12 +41,11 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo, abraços", new AuthorDTO(maria) );
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Partiu Viagem", "Acordei feliz hoje!", new AuthorDTO(maria));
-				
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
-		
-	}
-	
-	
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		userRepository.save(maria);
 
+	}
 }
