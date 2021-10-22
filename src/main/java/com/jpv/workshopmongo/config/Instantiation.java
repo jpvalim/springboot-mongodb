90eebbc6bf44489f00b66ded72f85e02b50a5029
Jeff Valim <jpvalim@hotmail.com>
2021-10-21 22:21:04 -0300

    Simple search with query methods

--- src/main/java/com/jpv/workshopmongo/config/Instantiation.java
+++ src/main/java/com/jpv/workshopmongo/config/Instantiation.java
@@ -40,7 +40,7 @@ public class Instantiation implements CommandLineRunner{
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo, abraços", new AuthorDTO(maria) );
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Partiu Viagem", "Acordei feliz hoje!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
 		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
